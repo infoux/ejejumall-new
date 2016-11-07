eb48649c321440cdd177923a97d892237eb2a2c2
rhkanrdl <emrdl7@gmail.com>
2016-11-07 18:08:22 +0900

    메인 코딩 중...

--- js/common.js
+++ js/common.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
 
+    $('button.all-menu').on('click', function() {
+        $(".menu-set").toggleClass("on");
+        $(this).toggleClass("on");
+        $(this).find("i").toggleClass("fa-bars").toggleClass("fa-close");
+    });
 
+    var issue = $('.main-issue .banner .slide').bxSlider({
+        pagerCustom: '.banner-pager',
+        controls:false,
+        infiniteLoop:true,
+        auto:true
+    });
+
+    var minibanner = $('.main-issue .s-banner .mini-banner ul').bxSlider({
+        controls:false,
+        infiniteLoop:true,
+        auto:true
+    });
 
 
 });
-
